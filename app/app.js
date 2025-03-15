ade2da9a8f14bee406cae364cdcea6302e213e21
izgrng <ishmagurung48@gmail.com>
2025-03-15 20:49:42 +0000

    Added features for outfits

--- app/app.js
+++ app/app.js
@@ -14,26 +14,42 @@ app.set('views', './views');
 
 // Route to render the Access Page
 app.get("/access", (req, res) => {
-    res.render("access"); // This will render "views/access.pug"
+    res.render("access"); 
 });
 
 
 // Route to render the home page
 app.get("/home", (req, res) => {
-    res.render("home"); // This assumes "fashion.pug" is in the "views" folder
+    res.render("home"); // This assumes "home.pug" is in the "views" folder
 });
 
 
 // Route to render the explore page
 app.get("/explore", (req, res) => {
-    res.render("explore"); // This assumes "fashion.pug" is in the "views" folder
+    res.render("explore"); // This assumes "explore.pug" is in the "views" folder
 });
 
-
+// To check if its working or not 
 app.get("/", (req, res) => {
-    res.send("Welcome to the home page!"); // Send a response to the client
+    res.send("Welcome to the fashion Page!"); // Send a response to the client
 });
 
+
+// Route to render the shared collection page
+app.get("/sharedcollection", (req, res) => {
+    res.render("sharedcollection"); // This assumes "sharedcollection.pug" is in the "views" folder
+});
+
+
+// Route to render the userlist page
+app.get("/userlist", (req, res) => {
+  res.render("userlist"); // Passing the users data to the template
+});
+
+// Route to render the detail page
+app.get("/detail", (req, res) => {
+    res.render("detail"); // Passing the users data to the template
+});
 
 // Start server on port 3000
 app.listen(3000, (err) => {
