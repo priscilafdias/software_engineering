b56754791e7610546be1fa65eacb1dcb0052c3be
izgrng <ishmagurung48@gmail.com>
2025-04-05 12:38:56 +0100

    Database created

--- sd2-db.sql
+++ sd2-db.sql
@@ -97,3 +97,13 @@ CREATE TABLE upload_outfit (
     imageUrl VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE messages (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  sender_id INT NOT NULL,
+  receiver_id INT NOT NULL,
+  content TEXT NOT NULL,
+  timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (sender_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
+);
